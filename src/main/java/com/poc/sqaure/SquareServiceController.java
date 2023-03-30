f7ac53c8fe70584a37f9b2b591ad6257712b25dd
Siva Kumar Gampala <sivakumarg2009@gmail.com>
2023-03-30 13:04:30 +0530

    initial commit

---
+++ src/main/java/com/poc/sqaure/SquareServiceController.java
@@ -0,0 +1,20 @@
+package com.poc.sqaure;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SquareServiceController {
+	
+	@GetMapping("/square/{inputNum}")
+	public double squareOfNum(@PathVariable double inputNum)
+	{
+		
+		System.out.println("Received number is " + inputNum);
+		double result = Math.pow(inputNum, 2);
+		return result;
+		
+	}
+
+}
---
+++ src/test/java/com/poc/sqaure/SquareServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.sqaure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
