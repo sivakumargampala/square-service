9cd69c0c10402465fd8c77c93595a6c7c3382047
Siva Kumar Gampala <sivakumarg2009@gmail.com>
2023-04-03 15:19:27 +0530

    Update SquareServiceController.java
    cross

--- src/main/java/com/poc/sqaure/SquareServiceController.java
+++ src/main/java/com/poc/sqaure/SquareServiceController.java
@@ -1,10 +1,12 @@
 package com.poc.sqaure;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class SquareServiceController {
 	
 	@GetMapping("/square/{inputNum}")
